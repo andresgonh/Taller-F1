e07309f3b17370d5999a78350f5bcb0376a2fbd7
andresgonh <andresgonh>
2024-10-26 14:34:09 +0000

    lineas
    lineas

--- Cuaderno sin nombre 2024-10-26 08_36_14.py
+++ Cuaderno sin nombre 2024-10-26 08_36_14.py
@@ -1,2 +1,20 @@
 # Databricks notebook source
-print("Hola")
+# MAGIC %md
+# MAGIC **Importación de la librerias necesarias**
+
+# COMMAND ----------
+
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+import uuid
+
+# COMMAND ----------
+
+# Variables de ubicación de archivos
+dl_location = 'abfss://dataengineering@bidatarepositoryg3agonza.dfs.core.windows.net/'
+raw_location = dl_location + 'RAW/'
+
+uc_location_aumented = 'medallion_architecture.aumented.'
+
+# Otras variables
+date_format = 'dd/MM/yyyy'
